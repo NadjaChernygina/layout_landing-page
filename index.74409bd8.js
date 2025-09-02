8799239322bc4ae3427c5e47065b549ef3e135b1
Nadiia Chernygina <nadja.chernygina@gmail.com>
2025-09-02 16:45:07 +0300

    make build

---
+++ index.74409bd8.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll(".section"),t=new IntersectionObserver(function(e){e.forEach(function(e){e.isIntersecting&&e.target.classList.add("visible")})},{threshold:.2});e.forEach(function(e){return t.observe(e)});
+//# sourceMappingURL=index.74409bd8.js.map
